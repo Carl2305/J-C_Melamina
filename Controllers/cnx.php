a293d823cc7feaa9fbbb0fca043a62a6d1ed3615
CARLOS MOGOLLON ESPINOZA <mogollon.23carlos05@gmail.com>
2022-01-23 16:33:51 -0500

    Update cnx.php

--- Controllers/cnx.php
+++ Controllers/cnx.php
@@ -1,16 +1,5 @@
 <?php
 error_reporting(0);
-//conexion para el servidor remoto 000webhostapp.com
-/*$link='mysql:host=localhost;dbname=id16497396_jcmelamina';
-$user='id16497396_root';
-$password='_JZC1De+-26EiT\i';
-try{
-	$pdo=new PDO($link, $user, $password);
-	//echo 'conectado';	
-}catch(PDOException $e){
-	print '¡Error!: ' . $e->getMessage() . "<br/>";
-	die();
-}*/
 //conexion para la BD en localhost
 
 $servidor='mysql:host=localhost;dbname=jcmelamina';
